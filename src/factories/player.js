e071259ea4ba980b33b46c719498a1526d1d3e9d
Sumaer Jolly <sumaerjolly@gmail.com>
2020-01-15 14:42:35 +0530

    modified player factory code

--- src/factories/player.js
+++ src/factories/player.js
@@ -1,4 +1,8 @@
-const player = (board, name) => {
-	return { board, name };
+const player = name => {
+  const randomMove = () => {
+    return Math.floor(Math.random() * 100);
+  };
+
+  return { name, randomMove };
 };
 export default player;
