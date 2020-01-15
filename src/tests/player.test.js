5f1bbbf33366890928499c053fc3ab51a1c7e9a5
Sumaer Jolly <sumaerjolly@gmail.com>
2020-01-15 14:51:04 +0530

    modified tests for player

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,21 +1,15 @@
 import player from '../factories/player';
-
-const board = [2, 4, 5, 4, 6, 8, 1, 0, 9, 10];
-const name = 'Bright';
-const newPlayer = player(board, name);
-
-test('test for player board', () => {
-	expect(newPlayer.board).toEqual(board);
-});
-
-test('test for player board length', () => {
-	expect(newPlayer.board.length).toEqual(10);
-});
+const name = 'Human';
+const newPlayer = player(name);
 
 test('test for player name', () => {
-	expect(newPlayer.name).toEqual(name);
+  expect(newPlayer.name).toEqual(name);
 });
 
-test('test for player', () => {
-	expect(newPlayer).toEqual({ board, name });
+test('gives a random number greater than or equal to 0', () => {
+  expect(newPlayer.randomMove()).toBeGreaterThanOrEqual(0);
+});
+
+test('gives a random number less than 100', () => {
+  expect(newPlayer.randomMove()).toBeLessThan(100);
 });
