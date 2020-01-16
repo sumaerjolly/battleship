24b299315fbb6ead6be0372a16e72cae9d873baa
Sumaer Jolly <sumaerjolly@gmail.com>
2020-01-16 14:48:23 +0530

    game logic dom added

--- src/dom.js
+++ src/dom.js
@@ -5,12 +5,26 @@ const start = (() => {
   const board = gameBoard();
   const user = player('user');
   const computer = player('computer');
+  const displayInfo = document.querySelector('.oppInfo');
+  const update = document.querySelector('.oppUpdate');
+  const myUpdate = document.querySelector('.myUpdate');
+  const myInfo = document.querySelector('.myInfo');
   let currPlayer = user;
   let gameStop = false;
   let userCount = 0;
   let computerCount = 0;
 
-  const game = () => {};
+  const game = () => {
+    const oppositionShips = document.querySelectorAll('.oppShips');
+    oppositionShips.forEach(element => {
+      const position = element.getAttribute('id');
+      element.addEventListener('click', e => {
+        e.preventDefault();
+        if (!gameStop && element.innerHTML === '') {
+        }
+      });
+    });
+  };
 
   return { board };
 })();
